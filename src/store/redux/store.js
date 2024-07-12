fce24d9067de1f896fa17a505eb71936afcfbe3a
benny hanania <bennyha@bennys-Air-2.lan>
2024-07-12 11:18:34 +0300

    small fix

--- src/store/redux/store.js
+++ src/store/redux/store.js
@@ -1,6 +1,5 @@
 const redux = require('redux');
 import reducer from './reducer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const store = redux.createStore(reducer);
 
@@ -8,11 +7,5 @@ const counterSubscriber = () => {
     const latestState = store.getState();
     console.log(latestState);
 }
-
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  blacklist: [],
-};
 
 store.subscribe(counterSubscriber);
